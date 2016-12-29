cd0ea5674846dda249c0bed6e1d8ac34fe887ad7
duytri <duytri2712@gmail.com>
2016-12-29 14:09:26 +0700

    Fix some bugs

--- src/main/scala/LDADataFile2OrientDBGraph.scala
+++ src/main/scala/LDADataFile2OrientDBGraph.scala
@@ -13,18 +13,21 @@ import java.util.HashSet
 import java.lang.Iterable
 import com.tinkerpop.blueprints.Edge
 import com.tinkerpop.blueprints.Direction
+import java.io.File
 
 object LDADataFile2OrientDBGraph {
 
   def main(args: Array[String]): Unit = {
-    val rootDir = "C:\\Users\\duytr\\Desktop\\CayChuDe_GiaoDuc\\CayChuDe_GiaoDuc"
+    //val rootDir = "C:\\Users\\duytr\\Desktop\\CayChuDe_GiaoDuc\\CayChuDe_GiaoDuc"
+    val rootDir = "/home/duytri/Desktop/CayChuDe_GiaoDuc"
     val odbName = "ODBGiaoDuc"
     val topicTopicEdgeLabel = "coChuDe"
     val topicWordEdgeLabel = "coTuKhoa"
     val dataFileName = "DataTrain.txt"
 
     // opens the DB (if not existing, it will create it)
-    val uri: String = "plocal:C:/orientdb/databases/" + odbName
+    //val uri: String = "plocal:C:/orientdb/databases/" + odbName
+    val uri: String = "plocal:/home/duytri/orientdb/databases/" + odbName
     val factory: OrientGraphFactory = new OrientGraphFactory(uri)
     val graph: OrientGraph = factory.getTx()
 
@@ -59,19 +62,24 @@ object LDADataFile2OrientDBGraph {
       val topicGiaoDuc: Vertex = graph.addVertex("class:CHUDE", "TenChuDe", "Giáo dục")
 
       val listFoldersLevel1 = Utils.getListOfFolders(rootDir)
+      println("Giao duc co " + listFoldersLevel1.length + " chu de con.")
       listFoldersLevel1.foreach { folder1 =>
         {
+          println("Chu de (cap 1): " + folder1.getName)
           //add vertex of topic and edge between them
           val topicLevel1: Vertex = graph.addVertex("class:CHUDE", "TenChuDe", folder1.getName)
           topicGiaoDuc.addEdge(topicTopicEdgeLabel, topicLevel1)
           graph.commit()
           //add vertex of words and edge between word and topic
-          val dataFileDir = folder1.getAbsolutePath + "\\" + dataFileName
-          println(dataFileDir)
+          val dataFileDir = folder1.getAbsolutePath + File.pathSeparator + dataFileName
+          println("Duong dan file chua du lieu: " + dataFileDir)
           val dataLines = Utils.getLines(dataFileDir)
+          var wordCount1 = 0
+          dataLines.remove(0)
           dataLines.foreach { line =>
             {
               val words = line.split(" ")
+              wordCount1 = wordCount1 + words.length
               words.foreach { word =>
                 {
                   val wordLevel1: Vertex = graph.addVertex("class:TUKHOA", "NoiDung", word)
@@ -81,20 +89,27 @@ object LDADataFile2OrientDBGraph {
               }
             }
           }
+          println("So tu khoa trong chu de nay la: " + wordCount1)
+          // jump into level 2
           val listFoldersLevel2 = Utils.getListOfFolders(folder1.getAbsoluteFile)
           if (listFoldersLevel2.length > 0)
             listFoldersLevel2.foreach { folder2 =>
               {
+                println("--- Chu de (cap 2): " + folder2.getName)
                 //add vertex of topic and edge between them
                 val topicLevel2: Vertex = graph.addVertex("class:CHUDE", "TenChuDe", folder2.getName)
                 topicLevel1.addEdge(topicTopicEdgeLabel, topicLevel2)
                 graph.commit()
                 //add vertex of words and edge between word and topic
-                val dataFileDir = folder2.getAbsolutePath + "\\" + dataFileName
+                val dataFileDir = folder2.getAbsolutePath + File.pathSeparator + dataFileName
+                println("--- Duong dan file chua du lieu: " + dataFileDir)
                 val dataLines = Utils.getLines(dataFileDir)
+                var wordCount2 = 0
+                dataLines.remove(0)
                 dataLines.foreach { line =>
                   {
                     val words = line.split(" ")
+                    wordCount2 = wordCount2 + words.length
                     words.foreach { word =>
                       {
                         val wordLevel2: Vertex = graph.addVertex("class:TUKHOA", "NoiDung", word)
@@ -104,20 +119,27 @@ object LDADataFile2OrientDBGraph {
                     }
                   }
                 }
+                println("--- So tu khoa trong chu de nay la: " + wordCount2)
+                // jump into level 3
                 val listFoldersLevel3 = Utils.getListOfFolders(folder2.getAbsoluteFile)
                 if (listFoldersLevel3.length > 0)
                   listFoldersLevel3.foreach { folder3 =>
                     {
+                      println(">>>>>> Chu de (cap 3): " + folder3.getName)
                       //add vertex of topic and edge between them
                       val topicLevel3: Vertex = graph.addVertex("class:CHUDE", "TenChuDe", folder3.getName)
                       topicLevel2.addEdge(topicTopicEdgeLabel, topicLevel3)
                       graph.commit()
                       //add vertex of words and edge between word and topic
-                      val dataFileDir = folder3.getAbsolutePath + "\\" + dataFileName
+                      val dataFileDir = folder3.getAbsolutePath + File.pathSeparator + dataFileName
+                      println(">>>>>> Duong dan file chua du lieu: " + dataFileDir)
                       val dataLines = Utils.getLines(dataFileDir)
+                      var wordCount3 = 0
+                      dataLines.remove(0)
                       dataLines.foreach { line =>
                         {
                           val words = line.split(" ")
+                          wordCount3 = wordCount3 + words.length
                           words.foreach { word =>
                             {
                               val wordLevel3: Vertex = graph.addVertex("class:TUKHOA", "NoiDung", word)
@@ -127,6 +149,7 @@ object LDADataFile2OrientDBGraph {
                           }
                         }
                       }
+                      println(">>>>>> So tu khoa trong chu de nay la: " + wordCount3)
                     }
                   }
               }
